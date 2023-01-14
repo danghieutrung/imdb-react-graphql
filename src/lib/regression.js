958a5cb2c6c2db8ea3f7dc71fdf46b426244cf8d
Kien Dang <mail@kien.ai>
2023-01-15 00:09:14 +0800

    Refactor regression (cont.)

--- src/lib/regression.js
+++ src/lib/regression.js
@@ -1,12 +1,9 @@
 import { sum, mean, range } from '../utils'
 
-const regressionLine = (episodes) => {
-  const length = episodes.length
+const line = (x, y, length = null) => {
+  const len = length ?? x.length
 
-  const x = episodes.map(episode => episode[0])
-  const y = episodes.map(episode => episode[1])
-
-  const coefs = regressionCoefficients(x, y, length)
+  const coefs = coefficients(x, y, len)
 
   const firstX = x[0]
   const lastX = x[length - 1]
@@ -14,7 +11,7 @@ const regressionLine = (episodes) => {
   return [[firstX, calculateYFromX(firstX, coefs)], [lastX, calculateYFromX(lastX, coefs)]]
 }
 
-const regressionCoefficients = (x, y, length = null) => {
+const coefficients = (x, y, length = null) => {
   const len = length ?? x.length
 
   const xMean = mean(x, len)
@@ -30,4 +27,4 @@ const regressionCoefficients = (x, y, length = null) => {
 
 const calculateYFromX = (x, [slope, intercept]) => (x * slope + intercept)
 
-export { regressionLine }
+export default { coefficients, line }
