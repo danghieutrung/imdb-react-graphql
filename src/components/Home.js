2f72cdcde1d068972ec5f1c9f3637cec42992ac2
Dang Trung Hieu <hieudangtrung@protonmail.com>
2023-01-16 20:57:53 +0800

    Add Home view

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import SearchForm from "./SearchForm";
+
+const Home = () => {
+  return (
+    <div>
+      <div><h1 id="menu-heading">IMDB-GraphQL</h1></div>
+      <SearchForm />
+    </div>
+  )
+}
+
+export default Home
