906fea3cebd426769f42cb6a9cecd59ad5429fc0
Dang Trung Hieu <hieudangtrung@protonmail.com>
2023-01-16 20:51:19 +0800

    Add view for Menu.js

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import {
   BrowserRouter as Router,
   Routes, Route
 } from "react-router-dom"
-import SearchForm from "./components/SearchForm"
 import SearchResults from "./components/SearchResults"
 import Chart from "./components/Chart"
+import Menu from "./components/Menu"
 
 const App = () => {
   return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<SearchForm/>} />
-          <Route path="/find" element={<SearchResults/>} />
-          <Route path="/chart/:imdbID" element={<Chart />} />
-        </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Menu />} />
+        <Route path="/find" element={<SearchResults />} />
+        <Route path="/chart/:imdbID" element={<Chart />} />
+      </Routes>
+    </Router>
   )
 }
 
