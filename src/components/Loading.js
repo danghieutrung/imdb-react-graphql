2c054f23162b378937fdc0980caba170a2f54086
Dang Trung Hieu <hieudangtrung@protonmail.com>
2023-01-17 07:49:07 +0800

    Add view for Loading

---
+++ src/components/Loading.js
@@ -0,0 +1,12 @@
+import SearchForm from "./SearchForm"
+
+const Loading = () => {
+  return (
+    <div>
+      <SearchForm />
+      <h1>Loading...</h1>
+    </div>
+  )
+}
+
+export default Loading
